f4f1e3abb3284b4a88e5d2c0d75253922592caf0
Schnelltoeter <86150934+Schnelltoeter@users.noreply.github.com>
2024-01-04 14:08:13 +0100

    forgot .ts now added

--- sieve/sieve.ts
+++ sieve/sieve.ts
@@ -1,69 +1,76 @@
-import * as fs from "fs";
-import { PrimeNumbers } from "../factor/factor.js";
-
-console.time("sieve");
-
-const max: number = 1000000;
-const primes: number[] = [];
-const sieve: boolean[] = [];
-const primeNumbers: PrimeNumbers[] = [];
-var increment: number = 0;
-
-for (let number = 2; number < max + 1; number++) {
-    sieve.push(true);
+import { writeFileSync } from "fs";
+var max = 1000000;
+var runs = 100;
+var durations = [];
+for (var i = 0; i < runs; i++) {
+    const start = performance.now();
+    runSieve(max);
+    const end = performance.now();
+    durations[i] = end - start;
 }
+var primeNumbers = runSieve(max);
 
-for (let index = 0; index < Math.sqrt(sieve.length); index++) {
-    if (sieve[index]) {
-        increment = index + 2;
-        for (
-            let noPrimePos = index + increment;
-            noPrimePos < sieve.length;
-            noPrimePos = noPrimePos + increment
-        ) {
-            sieve[noPrimePos] = false;
+writeFileSync("./sieve/sieve_primenumbers.json", JSON.stringify(primeNumbers));
+writeFileSync(
+    "./sieve/sieve_primeneighbors.json",
+    JSON.stringify(printPrimeNeighbors(primeNumbers))
+);
+var averageTime = 0;
+durations.forEach((duration) => (averageTime += duration));
+averageTime /= runs;
+durations = durations.sort((a, b) => (a < b ? -1 : 1));
+console.log(`Min: ${durations[0].toFixed(10)} ms`);
+console.log(`Max: ${durations[durations.length - 1].toFixed(10)} ms`);
+console.log(
+    `Median: ${durations[Math.floor(durations.length / 2)].toFixed(10)} ms`
+);
+console.log(`Average runtime: ${averageTime.toFixed(10)} ms`);
+// fs.writeFileSync("./sieve/sieve_primes.json", JSON.stringify(primes));
+
+function runSieve(max) {
+    var primes = [];
+    var sieve = Array(max - 2).fill(true);
+    var increment = 0;
+    // for (var number = 2; number < max + 1; number++) {
+    //     sieve.push(true);
+    // }
+    var noPrimePos = 0;
+    for (var index = 0; index < Math.sqrt(sieve.length); index++) {
+        if (sieve[index]) {
+            increment = index + 2;
+            for (
+                noPrimePos = index + increment;
+                noPrimePos < sieve.length;
+                noPrimePos = noPrimePos + increment
+            ) {
+                sieve[noPrimePos] = false;
+            }
         }
     }
-}
-
-for (let prime = 0; prime < sieve.length; prime++) {
-    if (sieve[prime]) {
-        primes.push(prime + 2);
+    for (var prime = 0; prime < sieve.length; prime++) {
+        if (sieve[prime]) {
+            primes.push(prime + 2);
+        }
     }
-}
-
-for (let index = 0; index < primes.length; index++) {
-    const num = primes[index];
-    var isPrime = false;
-    if (num + 2 == primes[index + 1]) {
-        isPrime = true;
+    var primeNumbers = Array(primes.length);
+    for (var index = 0; index < primes.length; index++) {
+        var num = primes[index];
+        var isPrime = num + 2 == primes[index + 1];
+        primeNumbers[index] = {
+            number: num,
+            isPrime: true,
+            neighbor: { number: num + 2, isPrime: isPrime },
+        };
     }
-    primeNumbers.push({
-        number: num,
-        isPrime: true,
-        neighbor: { number: num + 2, isPrime: isPrime },
-    });
+    return primeNumbers;
 }
 
-function printPrimeNeighbors() {
-    const primeNeighbors: PrimeNumbers[] = [];
-
-    primeNumbers.forEach((prime) => {
+function printPrimeNeighbors(primeNumbers) {
+    var primeNeighbors = [];
+    primeNumbers.forEach(function (prime) {
         if (prime.neighbor.isPrime === true) {
             primeNeighbors.push(prime);
         }
     });
-
     return primeNeighbors;
 }
-fs.writeFileSync(
-    "./sieve/sieve_primenumbers.json",
-    JSON.stringify(primeNumbers)
-);
-fs.writeFileSync(
-    "./sieve/sieve_primeneighbors.json",
-    JSON.stringify(printPrimeNeighbors())
-);
-
-// fs.writeFileSync("./sieve/sieve_primes.json", JSON.stringify(primes));
-console.timeEnd("sieve");
