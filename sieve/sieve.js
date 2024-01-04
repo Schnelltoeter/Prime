e6d873848e6d9040ea84c17037475a8a6aaf59c8
Schnelltoeter <86150934+Schnelltoeter@users.noreply.github.com>
2024-01-04 14:05:16 +0100

    improved sieve and analyze easier

--- sieve/sieve.js
+++ sieve/sieve.js
@@ -1,45 +1,73 @@
 // "use strict";
 // Object.defineProperty(exports, "__esModule", { value: true });
 import { writeFileSync } from "fs";
-console.time("sieve");
 var max = 1000000;
-var primes = [];
-var sieve = [];
-var primeNumbers = [];
-var increment = 0;
-for (var number = 2; number < max + 1; number++) {
-    sieve.push(true);
+var runs = 100;
+var durations = [];
+for (var i = 0; i < runs; i++) {
+    const start = performance.now();
+    runSieve(max);
+    const end = performance.now();
+    durations[i] = end - start;
 }
-for (var index = 0; index < Math.sqrt(sieve.length); index++) {
-    if (sieve[index]) {
-        increment = index + 2;
-        for (
-            var noPrimePos = index + increment;
-            noPrimePos < sieve.length;
-            noPrimePos = noPrimePos + increment
-        ) {
-            sieve[noPrimePos] = false;
+var primeNumbers = runSieve(max);
+
+writeFileSync("./sieve/sieve_primenumbers.json", JSON.stringify(primeNumbers));
+writeFileSync(
+    "./sieve/sieve_primeneighbors.json",
+    JSON.stringify(printPrimeNeighbors(primeNumbers))
+);
+var averageTime = 0;
+durations.forEach((duration) => (averageTime += duration));
+averageTime /= runs;
+durations = durations.sort((a, b) => (a < b ? -1 : 1));
+console.log(`Min: ${durations[0].toFixed(10)} ms`);
+console.log(`Max: ${durations[durations.length - 1].toFixed(10)} ms`);
+console.log(
+    `Median: ${durations[Math.floor(durations.length / 2)].toFixed(10)} ms`
+);
+console.log(`Average runtime: ${averageTime.toFixed(10)} ms`);
+// fs.writeFileSync("./sieve/sieve_primes.json", JSON.stringify(primes));
+
+function runSieve(max) {
+    var primes = [];
+    var sieve = Array(max - 2).fill(true);
+    var increment = 0;
+    // for (var number = 2; number < max + 1; number++) {
+    //     sieve.push(true);
+    // }
+    var noPrimePos = 0;
+    for (var index = 0; index < Math.sqrt(sieve.length); index++) {
+        if (sieve[index]) {
+            increment = index + 2;
+            for (
+                noPrimePos = index + increment;
+                noPrimePos < sieve.length;
+                noPrimePos = noPrimePos + increment
+            ) {
+                sieve[noPrimePos] = false;
+            }
         }
     }
-}
-for (var prime = 0; prime < sieve.length; prime++) {
-    if (sieve[prime]) {
-        primes.push(prime + 2);
+    for (var prime = 0; prime < sieve.length; prime++) {
+        if (sieve[prime]) {
+            primes.push(prime + 2);
+        }
     }
-}
-for (var index = 0; index < primes.length; index++) {
-    var num = primes[index];
-    var isPrime = false;
-    if (num + 2 == primes[index + 1]) {
-        isPrime = true;
+    var primeNumbers = Array(primes.length);
+    for (var index = 0; index < primes.length; index++) {
+        var num = primes[index];
+        var isPrime = num + 2 == primes[index + 1];
+        primeNumbers[index] = {
+            number: num,
+            isPrime: true,
+            neighbor: { number: num + 2, isPrime: isPrime },
+        };
     }
-    primeNumbers.push({
-        number: num,
-        isPrime: true,
-        neighbor: { number: num + 2, isPrime: isPrime },
-    });
+    return primeNumbers;
 }
-function printPrimeNeighbors() {
+
+function printPrimeNeighbors(primeNumbers) {
     var primeNeighbors = [];
     primeNumbers.forEach(function (prime) {
         if (prime.neighbor.isPrime === true) {
@@ -48,10 +76,3 @@ function printPrimeNeighbors() {
     });
     return primeNeighbors;
 }
-writeFileSync("./sieve/sieve_primenumbers.json", JSON.stringify(primeNumbers));
-writeFileSync(
-    "./sieve/sieve_primeneighbors.json",
-    JSON.stringify(printPrimeNeighbors())
-);
-// fs.writeFileSync("./sieve/sieve_primes.json", JSON.stringify(primes));
-console.timeEnd("sieve");
